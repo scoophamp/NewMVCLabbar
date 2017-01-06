9f6e0be517be64dcb5b4dc8b568948e57810fa39
Hampus <Hampus@DESKTOP-6CRJOHR>
2017-01-07 00:41:57 +0100

    update

---
+++ MVCLabs/MVCAjax/Scripts/LoadComment.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function (e) {
+
+    setInterval(reloadComments, 5000);
+});
+
+var reloadComments = function (e) {
+    $.ajax({
+        type: "GET",
+        url: "/Gallery/IndexPartial",
+        success: function (data) {
+            $('#content').html(data);
+        }
+    });
+};
---
+++ MVCLabs/MVCAjax/Scripts/MyMain.js
@@ -0,0 +1,22 @@
+﻿//Upload picture
+$(document).ready(function () {
+    var form = $("#uploadpicture");
+
+    form.submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: "/Gallery/UploadPicture",
+            data: new FormData(document.getElementsByTagName("form")[0]), beforeSend: function () {
+                $("#imgSpinner1").show();
+            },
+            success: function (data) {
+                $("#content").html(data);
+                $("#imgSpinner1").hide();
+
+            },
+            processData: false,
+            contentType: false
+        });
+    });
+});
